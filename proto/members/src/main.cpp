22a4b7b7346d95ba1fe1f8e9adc79aa2244feb02
Sean Dougherty <sean.dougherty.cs@gmail.com>
2015-01-10 09:51:08 -0500

    virtual function not defined in derivations

--- proto/members/src/main.cpp
+++ proto/members/src/main.cpp
@@ -16,33 +16,25 @@ struct NonPOD0 {
 };
 
 namespace __clunion {
-    typedef unsigned char vindex_t;
-
-    template<typename T> constexpr
-    T const& max(T const& a, T const& b) {
-        return a > b ? a : b;
-    }
-
     namespace clunion_A {
         namespace target {
             struct A;
         }
         namespace dispatch {
             void vecho(struct target::A *thiz);
+            void aecho(struct target::A *thiz);
         }
 
+        typedef unsigned char vindex_t;
+
         enum class Ids {
-            A, B
+            A, B, C
         };
 
         namespace target {
             struct A {
                 int a_i;
                 NonPOD0 np0;
-#ifdef DYNAMIC_TYPE_CHECKING
-                vindex_t __clunion_vindex_decl;
-#endif
-                vindex_t __clunion_vindex_actual;
 
                 A(int a_i_) : np0("np0") {
                     cout << "A(" << a_i_ << ")" << endl;
@@ -50,6 +42,12 @@ namespace __clunion {
                 }
                 ~A() {
                     cout << "~A()" << endl;
+                }
+                void aecho() {
+                    dispatch::aecho(this);
+                }
+                void __clunion_virtual_aecho() {
+                    cout << "aecho()" << endl;
                 }
                 void vecho() {
                     dispatch::vecho(this);
@@ -60,6 +58,11 @@ namespace __clunion {
                 void secho() {
                     cout << "A::secho()" << endl;
                 }
+
+#ifdef DYNAMIC_TYPE_CHECKING
+                vindex_t __clunion_vindex_decl;
+#endif
+                vindex_t __clunion_vindex_actual;
             };
 
             struct B : public A {
@@ -73,14 +76,32 @@ namespace __clunion {
                 ~B() {
                     cout << "~B()" << endl;
                 }
-                void vecho() {
-                    dispatch::vecho(this);
-                }
                 void __clunion_virtual_vecho() {
                     cout << "B::vecho(), a_i=" << a_i << ", b_i=" << b_i << endl;
                 }
                 void secho() {
                     cout << "B::secho()" << endl;
+                }
+            };
+
+            struct C : public A {
+                int c_i0;
+                int c_i1;
+                NonPOD0 np1;
+
+                C(int a_i_, int c_i0_, int c_i1_) : A(a_i_), np1("np1") {
+                    cout << "C(" << a_i_ << ", " << c_i0_ << ", " << c_i1_ << ")" << endl;
+                    c_i0 = c_i0_;
+                    c_i1 = c_i1_;
+                }
+                ~C() {
+                    cout << "~C()" << endl;
+                }
+                void __clunion_virtual_vecho() {
+                    cout << "C::vecho()(" << a_i << ", " << c_i0 << ", " << c_i1 << ")" << endl;
+                }
+                void secho() {
+                    cout << "C::secho()" << endl;
                 }
             };
 
@@ -107,11 +128,24 @@ namespace __clunion {
                         throw std::bad_cast();
                     }
                     break;
+                case Ids::C:
+                    if(to != Ids::C) {
+                        throw std::bad_cast();
+                    }
+                    break;
                 }
             }
 #endif
 
-            constexpr size_t sizeof_members = max(sizeof(A), sizeof(B));
+            template<typename T> constexpr
+            const T &max(const T &a, const T &b) {
+                return a > b ? a : b;
+            }
+            template<typename T, typename... U> constexpr
+            const T &max(const T &a, const U &... b) {
+                return max(a, max(b...));
+            }
+            constexpr size_t sizeof_members = max(sizeof(A), sizeof(B), sizeof(C));
         }
 
         namespace dispatch {
@@ -124,6 +158,9 @@ namespace __clunion {
                 }
                 void dtor(target::A *thiz) {
                     ((target::A *)thiz)->~A();
+                }
+                void aecho(target::A *thiz) {
+                    ((target::A *)thiz)->__clunion_virtual_aecho();
                 }
                 void vecho(target::A *thiz) {
                     ((target::A *)thiz)->__clunion_virtual_vecho();
@@ -145,10 +182,26 @@ namespace __clunion {
                 }
             };
 
+            namespace C {
+                void copy(target::A *thiz, const target::A &other) {
+                    new ((target::C *)thiz) target::C((const target::C&)other);
+                }
+                void assign(target::A *thiz, const target::A &other) {
+                    *((target::C *)thiz) = (const target::C&)other;
+                }
+                void dtor(target::A *thiz) {
+                    ((target::C *)thiz)->~C();
+                }
+                void vecho(target::A *thiz) {
+                    ((target::C *)thiz)->__clunion_virtual_vecho();
+                }
+            };
+
             struct Vtable {
                 void (*copy)(target::A*,const target::A&);
                 void (*assign)(target::A*,const target::A&);
                 void (*dtor)(target::A*);
+                void (*aecho)(target::A*);
                 void (*vecho)(target::A*);
             };
             Vtable vtables[] = {
@@ -156,13 +209,22 @@ namespace __clunion {
                     &A::copy,
                     &A::assign,
                     &A::dtor,
+                    &A::aecho,
                     &A::vecho
                 },
                 {
                     &B::copy,
                     &B::assign,
                     &B::dtor,
+                    &A::aecho,
                     &B::vecho
+                },
+                {
+                    &C::copy,
+                    &C::assign,
+                    &C::dtor,
+                    &A::aecho,
+                    &C::vecho
                 }
             };
 
@@ -172,6 +234,9 @@ namespace __clunion {
             }
             void dtor(target::A *thiz) {
                 vtables[(vindex_t)target::get_actual(thiz)].dtor(thiz);
+            }
+            void aecho(target::A *thiz) {
+                vtables[(vindex_t)target::get_actual(thiz)].aecho(thiz);
             }
             void vecho(target::A *thiz) {
                 vtables[(vindex_t)target::get_actual(thiz)].vecho(thiz);
@@ -255,28 +320,73 @@ namespace __clunion {
                     return *this;
                 }
             };
+
+            struct C : public A {
+            protected:
+                target::C *thiz() const {
+                    return (target::C *)A::thiz();
+                }
+            public:
+                C(const C &other) {
+#ifdef DYNAMIC_TYPE_CHECKING
+                    target::set_decl(thiz(), Ids::C);
+#endif
+                    dispatch::copy(thiz(), *other.thiz());
+                }
+                C(int a_i_, int c_i0_, int c_i1_) {
+#ifdef DYNAMIC_TYPE_CHECKING
+                    target::set_decl(thiz(), Ids::C);
+#endif
+                    target::set_actual(thiz(), Ids::C);
+                    new (thiz()) target::C(a_i_, c_i0_, c_i1_);
+                }
+                target::C *operator->() const {
+                    return thiz();
+                }
+                C &operator=(const C &other) {
+                    if(this != &other) {
+                        dispatch::assign(thiz(), *other.thiz());
+                    }
+                    return *this;
+                }
+            };
         }
     }
 }
 
 typedef __clunion::clunion_A::container::A A;
 typedef __clunion::clunion_A::container::B B;
+typedef __clunion::clunion_A::container::C C;
 
 int main(int argc, const char **argv) {
     B b{11,12};
+    b->aecho();
     b->secho();
     b->vecho();
 
     A &ar{b};
+    ar->aecho();
     ar->secho();
     ar->vecho();
 
     A a{42};
+    a->aecho();
     a->secho();
     a->vecho();
+
+    C c{101,102,103};
+    c->aecho();
+    c->secho();
+    c->vecho();
+
+    cout << "---" << endl;
+    a = c;
+    a->secho();
+    a->vecho();
+    cout << "---" << endl;
 }
 
-//todo: clunion C : public A
+// todo: move constructor
 
 /*
 clunion A {
@@ -291,6 +401,10 @@ clunion A {
         cout << "~A()" << endl;
     }
 
+    virtual void aecho() {
+        cout << "aecho()" << endl;
+    }
+
     virtual void vecho() {
         cout << "A::vecho(), a_i=" << a_i << endl;
     }
@@ -299,8 +413,7 @@ clunion A {
         cout << "A::secho()" << endl;
     }
 };
-*/
-/*
+
 clunion B : public A {
     int b_i;
     NonPOD0 np1;
@@ -309,13 +422,39 @@ clunion B : public A {
         cout << "B(" << a_i_ << ", " << b_i_ << ")" << endl;
         b_i = b_i_;
     }
+    ~B() {
+        cout << "~B()" << endl;
+    }
 
     virtual void vecho() {
-        cout << "B, a_i=" << a_i << ", b_i=" << b_i << endl;
+        cout << "B::vecho(), a_i=" << a_i << ", b_i=" << b_i << endl;
     }
 
     void secho() {
         cout << "B::secho()" << endl;
+    }
+};
+
+clunion C : public A {
+    int c_i0;
+    int c_i1;
+    NonPOD0 np1;
+
+    C(int a_i_, int c_i0_, int c_i1_) : A(a_i_), np1("np1") {
+        cout << "C(" << a_i_ << ", " << c_i0_ << ", " << c_i1_ << ")" << endl;
+        c_i0 = c_i0_;
+        c_i1 = c_i1_;
+    }
+    ~C() {
+        cout << "~C()" << endl;
+    }
+
+    virtual void vecho() {
+        cout << "C::vecho()(" << a_i << ", " << c_i0 << ", " << c_i1 << ")" << endl;
+    }
+
+    void secho() {
+        cout << "C::secho()" << endl;
     }
 };
 */
